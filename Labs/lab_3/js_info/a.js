220fb21cd128f95c27daea08a1bc354a6a1c23a1
Adil <adilzhapar2002@gmail.com>
2022-02-13 23:00:30 +0600

    upd

---
+++ Labs/lab_3/js_info/a.js
@@ -0,0 +1,13 @@
+alert("I'm JavaScript");
+
+var admin;
+var name = "John";
+admin = name;
+alert(admin)
+
+const eart_name = "Earth";
+let visitor = 'Someone';
+
+const BIRTHDAY = '13.08.2002';
+const age = somecode(BIRTHDAY);
+
