984f0c37c53b870035b75d2c9d57ee93108938e7
Adil <adilzhapar2002@gmail.com>
2022-02-18 12:52:38 +0600

    upd

--- Labs/lab_3/todo_list/code.js
+++ Labs/lab_3/todo_list/code.js
@@ -1,33 +1,34 @@
 let list = document.querySelector(".list");
 
 function add(){
-    let v = document.createElement("div")
-    v.className = "task"
     let input = document.querySelector("#in");
+    if(input.value != ""){
+        let v = document.createElement("div")
+        v.className = "task"
 
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.className = "nura";
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.className = "nura";
 
-    let p = document.createElement("p");
-    p.innerHTML = input.value;
-    
-    let img = document.createElement("img");
-    img.id = "trash";
-    img.src = "https://i.ibb.co/hFHdy3D/Pngtree-black-trash-can-free-illustration-4601444.png";
-    img.style.width = "50px";
-    
+        let p = document.createElement("p");
+        p.innerHTML = input.value;
+        
+        let img = document.createElement("img");
+        img.id = "trash";
+        img.src = "https://i.ibb.co/hFHdy3D/Pngtree-black-trash-can-free-illustration-4601444.png";
+        img.style.width = "50px";
+        
 
-    v.appendChild(checkbox);
-    v.appendChild(p);
-    v.appendChild(img);
-    list.appendChild(v);
-    input.value = "";
+        v.appendChild(checkbox);
+        v.appendChild(p);
+        v.appendChild(img);
+        list.appendChild(v);
+        input.value = "";
 
-    img.addEventListener("click", remove);
-    // document.getElementById("trash").onclick = function() {
-    //     this.parentNode.remove();
-    // }
+        img.addEventListener("click", remove);
+    }else{
+        alert("Empty input!");
+    }
 }
 
 function remove(){
